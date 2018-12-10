5cba06c68ff34678ebffca9d2399fa190addb59e
Dave <dave@chatmost.com>
2018-12-09 20:26:20 -0800

    Use elliptic instead.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,37 @@
-import ecurve from 'ecurve'
+import elliptic from 'elliptic'
+import BN from 'bn.js'
 import sha256 from 'js-sha256'
-import BigInteger from 'bigi'
 import crypto from 'crypto'
+
 
 //
 // The NUMS computation of H.
 //
-const curve = ecurve.getCurveByName('secp256k1')
-const G = curve.G
- 
+const curve = elliptic.curves.secp256k1.curve
+const G = curve.g
+
+console.log(G.getX().toBuffer())
+
 // H is defined as H.x = sha256(G)[0...15], where G is
 // the uncompressed representation of G: 0x4 [x] [y]
 
-const uncompressedG = Buffer.concat([Buffer.alloc(1, 0x4), G.x.toBuffer(), G.y.toBuffer()])
-const Hx = sha256(uncompressedG)
+const uncompressedG = Buffer.concat([Buffer.alloc(1, 0x4), G.getX().toBuffer(), G.getY().toBuffer()])
+const rawH = sha256(uncompressedG).toString(16)
 
-const H = curve.pointFromX(false, BigInteger.fromHex(Hx))
-const uncompressedH = Buffer.concat([Buffer.alloc(1, 0x4), H.x.toBuffer(), H.y.toBuffer()])
+console.log(rawH.slice(0,32))
+console.log(rawH.slice(32))
 
+const H = curve.point(rawH.slice(0, 32), rawH.slice(32))
+
+console.log(H.getX().toBuffer())
+console.log(H.getY().toBuffer())
+
+const rn = new BN(crypto.randomBytes(32))
+console.log(rn)
 class Commitment {
   constructor(value, blinding) {
     this.value = value
-    this.blinding = blinding === undefined ? BigInteger.fromBuffer(crypto.randomBytes(32)) : blinding
+    this.blinding = blinding === undefined ? new BN(crypto.randomBytes(32)) : blinding
   }
 
   add(other) {
@@ -29,12 +39,13 @@ class Commitment {
   }
 
   sub(other) {
-    return new Commitment(this.value - other.value, this.blinding.subtract(other.blinding))
+    return new Commitment(this.value - other.value, this.blinding.sub(other.blinding))
   }
 
   commit() {
-    console.log(this.value)
-    return { G: G.multiply(this.blinding), H: H.multiply(new BigInteger(`${this.value}`)) }
+    const g = G.mul(this.blinding)
+    const h = H.mul(new BN(this.value))
+    return { blinding, value, g, h, isZero: h.inf  }
   }
 }
 
@@ -43,5 +54,8 @@ const v2 = new Commitment(30)
 const v3 = new Commitment(130)
 const sum = v1.add(v2).sub(v3)
 const commit = sum.commit()
-console.log(`${commit.G.x.toHex()}
-${commit.H.x}`)
+console.log(`${commit.G.getX().toBuffer().toString('hex')}
+${Object.keys(commit.H)}
+${commit.H.inf}
+${commit.G.inf}
+`)
