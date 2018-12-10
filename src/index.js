124d61610bc48b51fe6ad80661cabf1d141b2227
Dave <dave@chatmost.com>
2018-12-10 08:14:06 -0800

    Allow generators to multiply by negative scalars.

--- src/index.js
+++ src/index.js
@@ -10,42 +10,31 @@ import crypto from 'crypto'
 const curve = elliptic.curves.secp256k1.curve
 const G = curve.g
 
-console.log(G.getX().toBuffer())
-
 // H is defined as H.x = sha256(G)[0...15], where G is
 // the uncompressed representation of G: 0x4 [x] [y]
 
 const uncompressedG = Buffer.concat([Buffer.alloc(1, 0x4), G.getX().toBuffer(), G.getY().toBuffer()])
 const rawH = sha256(uncompressedG).toString(16)
 
-console.log(rawH.slice(0,32))
-console.log(rawH.slice(32))
-
 const H = curve.point(rawH.slice(0, 32), rawH.slice(32))
 
-console.log(H.getX().toBuffer())
-console.log(H.getY().toBuffer())
-
-const rn = new BN(crypto.randomBytes(32))
-console.log(rn)
 class Commitment {
-  constructor(value, blinding) {
-    this.value = value
-    this.blinding = blinding === undefined ? new BN(crypto.randomBytes(32)) : blinding
+  constructor(v, r) {
+    this.v = v
+    this.r = r === undefined ? new BN(crypto.randomBytes(32)) : r
+    // BN.js doesn't seem to handle multiplying by a negative scalar, so
+    // instead just negate the generator.
+    this.rG = this.r.isNeg() ? G.neg().mul(this.r.abs()) : G.mul(this.r)
+    this.vH = this.v < 0 ? H.neg().mul(Math.abs(this.v)) : H.mul(this.v)
+    this.isZero = this.vH.inf
   }
 
   add(other) {
-    return new Commitment(this.value + other.value, this.blinding.add(other.blinding))
+    return new Commitment(this.v + other.v, this.r.add(other.r))
   }
 
   sub(other) {
-    return new Commitment(this.value - other.value, this.blinding.sub(other.blinding))
-  }
-
-  commit() {
-    const g = G.mul(this.blinding)
-    const h = H.mul(new BN(this.value))
-    return { blinding, value, g, h, isZero: h.inf  }
+    return new Commitment(this.v - other.v, this.r.sub(other.r))
   }
 }
 
@@ -53,9 +42,9 @@ const v1 = new Commitment(100)
 const v2 = new Commitment(30)
 const v3 = new Commitment(130)
 const sum = v1.add(v2).sub(v3)
-const commit = sum.commit()
-console.log(`${commit.G.getX().toBuffer().toString('hex')}
-${Object.keys(commit.H)}
-${commit.H.inf}
-${commit.G.inf}
+console.log(`${v1.r} ${v2.r} ${v3.r} ${sum.r} ${sum.rG.isInfinity()}`)
+console.log(`${JSON.stringify(sum.rG)}`)
+console.log(`
+${sum.rG.getX().toBuffer().toString('hex')}
+${sum.isZero}
 `)
